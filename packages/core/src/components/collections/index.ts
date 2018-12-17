cc9d66c4bbf0787d6c784330e4a4aa5bd449aa98
Enrico Valbuena <enrico@valpunk.com>
2018-12-17 15:49:50 -0800

    quik fix

--- packages/core/src/components/collections/index.ts
+++ packages/core/src/components/collections/index.ts
@@ -1,4 +1,15 @@
 import SimpleTable from "./Table/SimpleTable";
 import TableWithModal from "./Table/TableWithModal";
+import TableCell from "./Table/components/TableCell";
+import TableHeaderCell from "./Table/components/TableHeaderCell";
+import TablePagination from "./Table/components/TablePagination";
+import TableToolbar from "./Table/components/TableToolbar";
 
-export { SimpleTable, TableWithModal };
+export {
+  SimpleTable,
+  TableWithModal,
+  TableCell,
+  TableHeaderCell,
+  TablePagination,
+  TableToolbar
+};
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,22 @@
 import { ButtonOutline, RegularButton } from "./components/basic/Button";
-import { SimpleTable, TableWithModal } from "./components/collections/";
+import {
+  SimpleTable,
+  TableWithModal,
+  TableCell,
+  TableHeaderCell,
+  TablePagination,
+  TableToolbar
+} from "./components/collections/";
 import { Navbar } from "./components/compositions/";
 
-export { ButtonOutline, RegularButton, SimpleTable, TableWithModal, Navbar };
+export {
+  ButtonOutline,
+  RegularButton,
+  SimpleTable,
+  TableWithModal,
+  Navbar,
+  TableCell,
+  TableHeaderCell,
+  TablePagination,
+  TableToolbar
+};
