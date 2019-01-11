bc665fe8cd8dc06656efa85da2bd82da19547981
Unknown <adam@valpunk.com>
2019-01-11 11:13:34 -0800

    Finished Mockup of Documentation Page

--- packages/site/components/universal/SideMenu/index.tsx
+++ packages/site/components/universal/SideMenu/index.tsx
@@ -1,27 +1,21 @@
+import { Hidden } from "@material-ui/core"
 import AppBar from "@material-ui/core/AppBar"
 import Button from "@material-ui/core/Button"
 import Divider from "@material-ui/core/Divider"
 import Drawer from "@material-ui/core/Drawer"
 import IconButton from "@material-ui/core/IconButton"
 import List from "@material-ui/core/List"
-import Menu from "@material-ui/core/Menu"
-import MenuItem from "@material-ui/core/MenuItem"
 import { createStyles, Theme, withStyles } from "@material-ui/core/styles"
 import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"
 import ChevronRightIcon from "@material-ui/icons/ChevronRight"
 import MenuIcon from "@material-ui/icons/Menu"
-import SettingsIcon from "@material-ui/icons/Settings"
+import SearchIcon from "@material-ui/icons/Search"
 import * as classNames from "classnames"
-import * as React from "react"
-// import { primaryColor } from "~/lib/theme/colors"
-// import { mailFolderListItems } from "./sideBarData"
-import { compose } from "recompose"
 import { withRouter, WithRouterProps } from "next/router"
-// import { Meta } from "~/components/basic"
-
-
+import * as React from "react"
+import { compose } from "recompose"
 
 const drawerWidth = 300
 
@@ -33,36 +27,36 @@ const styles = (theme: Theme) =>
       zIndex: 1,
       overflow: "hidden",
       position: "relative",
-      display: "flex",
+      display: "flex"
     },
     appBar: {
       zIndex: theme.zIndex.drawer + 1,
       transition: theme.transitions.create(["width", "margin"], {
         easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
+        duration: theme.transitions.duration.leavingScreen
       }),
-      // backgroundColor: DARK_BLUE,
+      backgroundColor: "#142b47",
     },
     appBarShift: {
       marginLeft: drawerWidth,
       width: `calc(100% - ${drawerWidth}px)`,
       transition: theme.transitions.create(["width", "margin"], {
         easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
+        duration: theme.transitions.duration.enteringScreen
+      })
     },
     content: {
       flexGrow: 1,
       backgroundColor: theme.palette.background.default,
       padding: theme.spacing.unit * 3,
-      overflowY: "scroll",
+      overflowY: "scroll"
     },
     menuButton: {
       marginLeft: 12,
-      marginRight: 36,
+      marginRight: 36
     },
     hide: {
-      display: "none",
+      display: "none"
     },
     drawerPaper: {
       position: "relative",
@@ -70,27 +64,27 @@ const styles = (theme: Theme) =>
       width: drawerWidth,
       transition: theme.transitions.create("width", {
         easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
+        duration: theme.transitions.duration.enteringScreen
+      })
     },
     drawerPaperClose: {
       overflowX: "hidden",
       transition: theme.transitions.create("width", {
         easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
+        duration: theme.transitions.duration.leavingScreen
       }),
       width: theme.spacing.unit * 7,
       [theme.breakpoints.up("sm")]: {
-        width: theme.spacing.unit * 9,
-      },
+        width: theme.spacing.unit * 9
+      }
     },
     toolbar: {
       display: "flex",
       alignItems: "center",
       justifyContent: "flex-end",
       padding: "0 8px",
-      ...theme.mixins.toolbar,
-    },
+      ...theme.mixins.toolbar
+    }
   })
 
 interface ClassProps {
@@ -115,6 +109,7 @@ interface Props extends WithRouterProps {
 interface State {
   open: boolean
   anchorEl?: HTMLElement | undefined
+  search?: string
 }
 
 class SideMenu extends React.Component<Props, State> {
@@ -123,7 +118,20 @@ class SideMenu extends React.Component<Props, State> {
     this.state = {
       open: false,
       anchorEl: undefined,
+      search: ""
     }
+  }
+
+  public handleSearch = (_event: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  public handleSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+    this.setState({
+      search: event.target.value
+    })
+
   }
 
   public handleDrawerOpen = () => {
@@ -134,24 +142,8 @@ class SideMenu extends React.Component<Props, State> {
     this.setState({ open: false })
   }
 
-  public handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    this.setState({ anchorEl: event.currentTarget })
-  }
-
-  public handleClose = () => {
-    this.setState({ anchorEl: undefined })
-  }
-
-  public logout = async () => {
-    this.props.router.push("/")
-    // await this.props.client.resetStore()
-    localStorage.clear()
-  }
-
   public render() {
-    const { classes, theme, userName } = this.props
-    // const { classes, theme } = this.props
-    const open = Boolean(this.state.anchorEl)
+    const { classes, theme } = this.props
 
     return (
       <div>
@@ -162,109 +154,127 @@ class SideMenu extends React.Component<Props, State> {
           <AppBar
             position="absolute"
             className={classNames(
-              classes.appBar,
-              this.state.open && classes.appBarShift,
+              classes.appBar
             )}
           >
-            <Toolbar disableGutters={!this.state.open}>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={this.handleDrawerOpen}
-                className={classNames(
-                  classes.menuButton,
-                  this.state.open && classes.hide,
-                )}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography
-                variant="h6"
-                color="inherit"
-                noWrap={true}
-                style={{ flex: 1 }}
-              >
-                {userName}
-              </Typography>
-              <div>
-                {/* <Button
-                  aria-owns={open ? "menu-list-grow" : undefined}
-                  aria-haspopup="true"
-                  style={{ color: "white" }}
-                >
-                  Prson
-                </Button> */}
+            <Toolbar disableGutters={false}>
+              <Hidden mdUp>
                 <IconButton
                   color="inherit"
-                  aria-label="options"
-                  className={classNames(classes.menuButton)}
-                  onClick={this.handleMenu}
+                  aria-label="open drawer"
+                  onClick={this.handleDrawerOpen}
+                  className={classNames(
+                    classes.menuButton,
+                    this.state.open && classes.hide
+                  )}
                 >
-                  <SettingsIcon />
+                  <MenuIcon />
                 </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={this.state.anchorEl}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
+              </Hidden>
+
+              <div
+                style={{
+                  width: "100%",
+                  display: "inline-flex",
+                  justifyContent: "flex-end"
+                }}
+              >
+                <input
+                  type="search"
+                  placeholder="Search..."
+                  style={{ borderRadius: "4px", width: "80%" }}
+                  onChange={this.handleSearchInput}
+                />
+                <Button
+                  type="submit"
+                  onClick={this.handleSearch}
+                  style={{
+                    position: "relative",
+                    backgroundColor: "#2196F3",
+                    color: "white"
                   }}
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={open}
-                  onClose={this.handleClose}
                 >
-                  <MenuItem onClick={this.logout}>Log out</MenuItem>
-                </Menu>
+                  <SearchIcon />
+                </Button>
               </div>
+
+
             </Toolbar>
           </AppBar>
-          <Drawer
-            variant="permanent"
-            classes={{
-              paper: classNames(
-                classes.drawerPaper,
-                !this.state.open && classes.drawerPaperClose,
-              ),
-            }}
-            style={
-              !this.state.open
-                ? {
-                    position: "relative",
-                    whiteSpace: "nowrap",
-                    overflowX: "hidden",
-                  }
-                : {}
-            }
-            open={this.state.open}
-          >
-            <div className={classes.toolbar}>
-              <Typography
-                variant="h6"
-                color="inherit"
-                noWrap={true}
-                style={{ flex: 1, color: "white", textAlign: "center" }}
-              >
-                {this.props.businessName}
-              </Typography>
-              <IconButton onClick={this.handleDrawerClose}>
-                {theme.direction === "rtl" ? (
-                  <ChevronRightIcon />
-                ) : (
-                  <ChevronLeftIcon />
-                )}
-              </IconButton>
-            </div>
-            <Divider />
-            <List>{this.props.listItems}</List>
-          </Drawer>
+          <Hidden mdDown>
+            <Drawer
+              variant="permanent"
+              classes={{
+                paper: classNames(classes.drawerPaper)
+              }}
+            >
+              <div className={classes.toolbar}>
+                <Typography
+                  variant="h6"
+                  color="inherit"
+                  noWrap={true}
+                  style={{ flex: 1, color: "white", textAlign: "center" }}
+                >
+                  {this.props.businessName}
+                </Typography>
+                <IconButton onClick={this.handleDrawerClose}>
+                  {theme.direction === "rtl" ? (
+                    <ChevronRightIcon />
+                  ) : (
+                    <ChevronLeftIcon />
+                  )}
+                </IconButton>
+              </div>
+              <Divider />
+              <List>{this.props.listItems}</List>
+            </Drawer>
+          </Hidden>
+
+          <Hidden mdUp>
+            <Drawer
+              variant="temporary"
+              classes={{
+                paper: classNames(
+                  classes.drawerPaper,
+                  !this.state.open && classes.drawerPaperClose
+                )
+              }}
+              style={
+                !this.state.open
+                  ? {
+                      position: "relative",
+                      whiteSpace: "nowrap",
+                      overflowX: "hidden"
+                    }
+                  : {}
+              }
+              open={this.state.open}
+            >
+              <div className={classes.toolbar}>
+                <Typography
+                  variant="h6"
+                  color="inherit"
+                  noWrap={true}
+                  style={{ flex: 1, color: "white", textAlign: "center" }}
+                >
+                  {this.props.businessName}
+                </Typography>
+                <IconButton onClick={this.handleDrawerClose}>
+                  {theme.direction === "rtl" ? (
+                    <ChevronRightIcon />
+                  ) : (
+                    <ChevronLeftIcon />
+                  )}
+                </IconButton>
+              </div>
+              <Divider />
+              <List>{this.props.listItems}</List>
+            </Drawer>
+          </Hidden>
+
           <main className={classes.content} style={{ overflowY: "scroll" }}>
             <div className={classes.toolbar} />
-            <div>
-              {this.props.children}
-            </div>
+            <div>{this.props.children}</div>
           </main>
         </div>
       </div>
@@ -274,7 +284,7 @@ class SideMenu extends React.Component<Props, State> {
 
 const enhance = compose(
   withStyles(styles, { withTheme: true }),
-  withRouter,
+  withRouter
 )
 
 const dashboardContainer = enhance(SideMenu)
--- packages/site/pages/docs/index.tsx
+++ packages/site/pages/docs/index.tsx
@@ -3,14 +3,14 @@ import * as React from "react";
 import { Layout, Meta } from "~/components/universal";
 import SideMenu from "~/components/universal/SideMenu";
 import Documentation from "./Documentation/Documentation";
+import { mockData } from "./mock"
 
 const listItems = (
   <List>
     <ListItem button={true}>Introduction</ListItem>
-    <ListItem button={true}>First Thing</ListItem>
-    <ListItem button={true}>Here is another Thing</ListItem>
+    <ListItem button={true}>API</ListItem>
+    <ListItem button={true}>FAQs</ListItem>
     <ListItem button={true}>Frequently Encountered Problems</ListItem>
-    <ListItem button={true}>Conclusion</ListItem>
   </List>
 )
 export default class Docs extends React.Component {
@@ -20,7 +20,7 @@ export default class Docs extends React.Component {
         <Meta />
         <Layout>
           <SideMenu listItems={listItems}>
-            <Documentation />
+            <Documentation data={mockData} />
           </SideMenu>
         </Layout>
       </div>
